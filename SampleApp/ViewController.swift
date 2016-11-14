288d16392b6d1f3ee035b240093e3c4aeb74f39e
Andy C <andy@andyclingman.com>
2016-11-13 23:01:05 -0500

    Initial commit!

--- SampleApp/ViewController.swift
+++ SampleApp/ViewController.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lowerBoundField: UITextField!
+    @IBOutlet var upperBoundField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //load the default values
+        lowerBoundField.text = "1"
+        upperBoundField.text = "10"
+        
+        //set the keyboard type
+        lowerBoundField.keyboardType = UIKeyboardType.numberPad
+        upperBoundField.keyboardType = UIKeyboardType.numberPad
+        
+        //tap outside text fields clears keyboard
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard)))
+    }
+    
+    func dismissKeyboard() {
+        lowerBoundField.resignFirstResponder()
+        upperBoundField.resignFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +39,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func generateTapped(_ sender: UIButton) {
+        if let lower = UInt32(lowerBoundField.text!), let upper = UInt32(upperBoundField.text!)
+        {
+            let randomNum = arc4random_uniform(upper - lower) + lower
+            
+            resultLabel.text = "Result: \(randomNum)"
+        }
+    }
 }
 
